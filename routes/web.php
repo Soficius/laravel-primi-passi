74c5f9e6e364f796c5c6a9b71cedabcfaac7db2f
Soficius <103771506+Soficius@users.noreply.github.com>
2022-09-15 09:57:11 +0200

    fixed error

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home', ['saluto' => 'Ciaoo mondoooo']);
+    $saluto = 'Ciaoo mondoooo';
+    return view('home', compact('saluto'));
 });
